81548c6cdfccc2864e11820a0d1d2edb4880e667
Amber Jones <amberlovescats14@gmail.com>
2019-06-25 13:13:44 -0500

    create exercise

---
+++ mern-exercise-tracker/src/components/ExerciseList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ExerciseList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ExerciseList</h1>
+      </div>
+    )
+  }
+}
